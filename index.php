d5644656ea6a8e2a2733483d09d38c6c457a02ba
Mika RAMPARANY <migame.strass@gmail.com>
2023-02-13 21:26:23 +0100

    Création $T1 $T2 $C1 $C2 + test echo get infoCompte pour tester l'affichage données bancaire

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <h1> Exercice POO Compte bancaire</h1>
 
+<br>
+On doit pouvoir :<br><br>
+Afficher toutes  les  informations  d'un(e)  titulaire  (dont  l'âge)<br>
+et  l'ensemble  des  comptes appartenant à celui(celle)-ci.<br>
+<br>
+Afficher  toutes  les  informations  d'un  compte  bancaire,  <br>
+notamment  le  nom  /  prénom  du titulaire du compte.<br></p>
 
 
+<h2> Résultat </h2>
 
 
 <?php 
 
+require "Titulaires.php";
+require "Comptebancaire.php";
+
+$T1 = new Titulaire('DUPONT', 'Jean','Paris', '12-12-1990');
+$T2 = new Titulaire('DUCHEMIN', 'Christiane','Paris', '23-04-1985');
+$C1 = new Comptebancaire( 'Compte Courant', $T1, '5000','€');
+$C2 = new Comptebancaire( 'Compte Courant', $T2, '15000','€');
+$C3 = new Comptebancaire( 'Compte Livret A', $T1, '30000','€');
+$C4 = new Comptebancaire( 'Compte Livret A', $T2, '5000','€');
 
 
-$compteCourant->virement(50, $compteEpargne);
+echo $C1-> get_Infocompte();
+
+
+// $compteCourant->virement(50, $compteEpargne);
