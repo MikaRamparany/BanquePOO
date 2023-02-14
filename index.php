0fa12f976cfe3284bc8dbbd7c98143a6120f9b9c
Mika RAMPARANY <migame.strass@gmail.com>
2023-02-14 09:56:05 +0100

    CRéation variable et echo pour Info compte, titulaire et virement

--- index.php
+++ index.php
@@ -21,11 +21,20 @@ $T1 = new Titulaire('DUPONT', 'Jean','Paris', '12-12-1990');
 $T2 = new Titulaire('DUCHEMIN', 'Christiane','Paris', '23-04-1985');
 $C1 = new Comptebancaire( 'Compte Courant', $T1, '5000','€');
 $C2 = new Comptebancaire( 'Compte Courant', $T2, '15000','€');
-$C3 = new Comptebancaire( 'Compte Livret A', $T1, '30000','€');
-$C4 = new Comptebancaire( 'Compte Livret A', $T2, '5000','€');
+$C1A = new Comptebancaire( 'Compte Livret A', $T1, '30000','€');
+$C2A = new Comptebancaire( 'Compte Livret A', $T2, '5000','€');
 
+//Infos et Mouvements Comptes DUPONT : 
 
-echo $C1-> get_Infocompte();
+echo $T1-> InfoTitulaire() ."<br>";
+echo $C1-> get_Infocompte()."<br>";
+echo $C1A -> get_Infocompte()."<br>";
+// echo$C1A-> debiter(99) ."<br>";
+// echo$C1-> crediter(99)."<br>";
+echo $C1 -> virement(1099,$C1A)."<br>";
 
+// Infos Mouvements Comptes DUCHEMIN : 
+echo $T2-> InfoTitulaire()."<br>";
+echo $C2-> get_Infocompte()."<br>";
 
 // $compteCourant->virement(50, $compteEpargne);
